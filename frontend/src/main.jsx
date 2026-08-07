3719661fa96eac7fe010c4d473a77e9f422d9679
Pabhishek-official <kumarkashebhik@gmail.com>
2026-08-07 23:14:07 +0530

    Add missing index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthProvider } from "./context/AuthContext";
-import './index.css'
+import "./index.css";
 import App from './App.jsx';
 
 import "bootstrap/dist/css/bootstrap.min.css";
