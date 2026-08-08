e85aa5e933f1c2fc645d1c3c941dd6550f09bd0f
Pabhishek-official <kumarkashebhik@gmail.com>
2026-08-08 21:11:36 +0530

    Add cloudinary image to storage

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,40 +1,16 @@
 const multer = require("multer");
-const path = require("path");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-//Storage configuration
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            Date.now() + path.extname(file.originalname).toLowerCase()
-        );
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "learninghub/courses",
+        allowed_formats: ["jpg", "jpeg", "png", "webp"]
     }
 });
-
-// file filter (only images allowed)
-const fileFilter = (req, file, cb) => {
-    const allowedExtensions = /jpeg|jpg|png|webp/;
-    const allowedMimeTypes = /image\/jpeg|image\/jpg|image\/png|image\/webp/;
-
-    const extension = allowedExtensions.test(
-        path.extname(file.originalname).toLowerCase()
-    );
-
-    const mimeType = allowedMimeTypes.test(file.mimetype);
-
-    if (extension && mimeType) {
-        cb(null, true);
-    } else {
-        cb(new Error("Only image files are allowed"));
-    }
-};
 
 const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter
+    storage: storage
 });
-
 module.exports = upload;
